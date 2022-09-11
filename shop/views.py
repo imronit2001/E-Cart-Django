e531357850b15131d09368d3317423a5645f0e5a
RONIT SINGH <ronitsingh7003@gmail.com>
2022-09-11 14:33:03 +0530

    #29 Exercise 3: List the Products

--- shop/views.py
+++ shop/views.py
@@ -3,24 +3,32 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
+from shop.models import Product
+
+
 def index(request):
     return render(request, 'shop/index.html')
+
+
+def productView(request):
+    return render(request, 'product.html', {'products': Product.objects.all()})
+
 
 def about(request):
     return HttpResponse("We are at about")
 
+
 def contact(request):
     return HttpResponse("We are at contact")
+
 
 def tracker(request):
     return HttpResponse("We are at tracker")
 
+
 def search(request):
     return HttpResponse("We are at search")
 
-def productView(request):
-    return HttpResponse("We are at product view")
 
 def checkout(request):
     return HttpResponse("We are at checkout")
-
